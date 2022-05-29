454e69aeb5f370ac3944235c603fefef395c1fa9
VinitGrover <vinitgrover255@gmail.com>
2022-05-29 19:11:27 +0530

    Add uc1

--- src/com/bridgelabz/AddressBook/AddressBookMain.java
+++ src/com/bridgelabz/AddressBook/AddressBookMain.java
@@ -1,7 +1,13 @@
 package com.bridgelabz.AddressBook;
-
 public class AddressBookMain {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
+        System.out.println("Welcome to the Address Book Main Program");
+
+        AddressBook a1 = new AddressBook();
+        a1.contactList();
+        System.out.println(a1.list);
+
+
     }
 }
