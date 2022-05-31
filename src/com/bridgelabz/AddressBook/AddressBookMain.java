22640712412cd520fdb80fe717944918d0c42911
VinitGrover <vinitgrover255@gmail.com>
2022-05-31 11:29:44 +0530

    added uc6

--- src/com/bridgelabz/AddressBook/AddressBookMain.java
+++ src/com/bridgelabz/AddressBook/AddressBookMain.java
@@ -1,4 +1,5 @@
 package com.bridgelabz.AddressBook;
+import java.util.*;
 
 public class AddressBookMain {
 
@@ -7,18 +8,19 @@ public class AddressBookMain {
 
         AddressBook a1 = new AddressBook();
         a1.contactList();
-        a1.contactList();
-       System.out.println(a1.list);
+        //a1.contactList();
+        System.out.println(a1.list);
 
-        System.out.println("if you want to change any data of a person, reply y/n : ");
+
+        System.out.println("if you want to remove contact of a person, reply y/n : ");
         String check = AddressBook.sc.nextLine();
 
 
         if (check.equals("y")) {
-        System.out.println("enter name whose details you want to edit : ");
-        String name = AddressBook.sc.nextLine();
+            System.out.println("enter name whose details you want to remove : ");
+            String name = AddressBook.sc.nextLine();
 
-            int i =0;
+            int i = 0;
             for (Contacts cThrough : a1.list) {
 
                 if (cThrough.getFirstName().equals(name)) {
@@ -28,6 +30,28 @@ public class AddressBookMain {
                 i++;
             }
         }
-        System.out.println(a1.list);
+
+        System.out.println("if you want to change any data of a person, reply y/n : ");
+        check = AddressBook.sc.nextLine();
+
+        if (check.equals("y")) {
+
+            System.out.println("enter name whose details you want to edit : ");
+            String name = AddressBook.sc.nextLine();
+
+            for (Contacts cThrough : a1.list) {
+
+                if (cThrough.getFirstName().equals(name)) {
+                    System.out.println("enter updated phone number : ");
+                    cThrough.setPhone(AddressBook.sc.next());
+                    break;
+                }
+            }
+        }
+
+        Map<String, AddressBook> map = new HashMap<>();
+        map.put("1",a1);
+        System.out.println(map.get(a1));
+
     }
 }
